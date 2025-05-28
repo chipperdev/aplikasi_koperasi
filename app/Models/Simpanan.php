1f12e489869429da8f561aca406f7a5c3c693c40
M. ERIC ARDIANSYAH <166952114+chipperdev@users.noreply.github.com>
2025-05-28 13:09:49 +0700

    model (simpanan)

--- app/Models/Simpanan.php
+++ app/Models/Simpanan.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Simpanan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'jenis',      // contoh: 'sukarela', 'pokok', dll
+        'tipe',       // 'rutin' atau 'non_rutin'
+        'jumlah',
+        'tanggal',
+        'keterangan',
+    ];
+
+    // Scope untuk simpanan sukarela rutin
+    public function scopeRutin($query)
+    {
+        return $query->where('jenis', 'sukarela')->where('tipe', 'rutin');
+    }
+
+    // Scope untuk simpanan sukarela non rutin
+    public function scopeNonRutin($query)
+    {
+        return $query->where('jenis', 'sukarela')->where('tipe', 'non_rutin');
+    }
 }
